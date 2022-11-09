ae7876b7b61bfb79e95f4fd697d51f95b6b31744
martin <mawisor@gmail.com>
2022-11-09 16:11:14 -0500

    second commit

--- makemoney.js
+++ makemoney.js
@@ -1,12 +1,17 @@
-function myFunction(){
-  let x = document.createElement("circle");
-  let t = document.createTextNode("circle");
-  x.appendChild(t);
-  document.body.appendChild(x)
-}
+const changeJar = document.querySelector(".change-jar");
+const moneyform = document.querySelector(".moneyform");
 
-
-function removeCircle(){
- document.getElementById("circle").addEventListener(click);
- circle.remove()
-}
+moneyform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let coinAmount = document.querySelector("#amount").value;
+  let coinChosen = document.querySelector("#type").value;
+  for (let i = 0; i < coinAmount; i++) {
+    const newCoin = document.createElement("li");
+    newCoin.classList.add("coin", coinChosen);
+    newCoin.textContent = coinChosen;
+    changeJar.append(newCoin);
+    newCoin.addEventListener("click", () => {
+      newCoin.remove();
+    });
+  }
+});
